a98ecb95a9d31e516682496cb5d80319e4fae4ba
Kushagra Agarwal <kushagraagarwal@Kushagras-MacBook-Pro.local>
2026-05-11 15:03:44 +0530

    Change heston_fft to take multiple strikes

--- cpp/include/heston_fft.h
+++ cpp/include/heston_fft.h
@@ -2,11 +2,13 @@
 #define HESTON_FFT_H
 
 #include <complex> // For std::complex
+#include <vector>  // For std::vector
 
 using complex = std::complex<double>;
+using vector = std::vector<double>;
 
 complex heston_cf(complex u, double S0, double v0, double r, double rho, double kappa, double theta, double xi, double T);
 complex heston_fourier_transform(double v, double alpha, double S0, double v0, double r, double rho, double kappa, double theta, double xi, double T);
-double heston_fft_price(double S0, double K, double v0, double eta, double alpha, double r, double rho, double kappa, double theta, double xi, double T, int N);
+vector heston_fft_price(double S0, const vector& K, double v0, double eta, double alpha, double r, double rho, double kappa, double theta, double xi, double T, int N);
 
 #endif
